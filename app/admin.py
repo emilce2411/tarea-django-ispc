8103305ab61e6bcc901d70e6a1bb0af9b993f9a6
emilce2411 <emilceagustinatorres@gmail.com>
2026-04-05 15:54:18 -0300

    Registro de modelos en admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, Role # Importamos tus modelos [cite: 1019]
+
+# Registramos para que aparezcan en el panel /admin [cite: 1021, 1022]
+admin.site.register(User)
+admin.site.register(Role)
